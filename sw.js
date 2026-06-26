a8955e896a6081a2cdd2de49f2112c7364fd222a
uruspotcdu-create <uruspotcdu@gmail.com>
2026-06-25 22:27:06 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
+});
